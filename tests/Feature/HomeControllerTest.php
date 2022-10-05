01627c25e2cde1927d5a7fca924c54a0de70d6ad
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-06 08:25:55 +0900

    TOPページ周りのテストを書いた

---
+++ tests/Feature/HomeControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function access_to_top_page()
+    {
+        User::factory()->create();
+        $user = User::first();
+
+        $this->get('/')->assertOk()->assertViewIs('top');
+        $this->actingAs($user)->get('/')->assertRedirect(route('home'));
+    }
+}
