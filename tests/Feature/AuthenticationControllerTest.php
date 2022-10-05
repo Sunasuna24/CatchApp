9052669ed49e1956acef3681dfab5c53d852c136
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-06 08:59:28 +0900

    アクセス周りのテストを作成した

---
+++ tests/Feature/AuthenticationControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AuthenticationControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function access_to_register_page()
+    {
+        User::factory()->create();
+        $user = User::first();
+
+        $this->get(route('register'))->assertOk()->assertViewIs('register');
+        $this->actingAs($user)->get(route('register'))->assertRedirect(route('home'));
+    }
+}
