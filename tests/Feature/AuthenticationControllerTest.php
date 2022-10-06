69f0997ed83b064bf7917bbfcc72d2081940e695
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-06 14:38:51 +0900

    会員登録するテストを追加した

--- tests/Feature/AuthenticationControllerTest.php
+++ tests/Feature/AuthenticationControllerTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class AuthenticationControllerTest extends TestCase
@@ -12,12 +13,33 @@ class AuthenticationControllerTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function access_to_register_page()
+    function access_to_register_page()
     {
         User::factory()->create();
         $user = User::first();
 
         $this->get(route('register'))->assertOk()->assertViewIs('auth.register');
         $this->actingAs($user)->get(route('register'))->assertRedirect(route('home'));
+    }
+
+    /** @test */
+    function register_to_application()
+    {
+        $valid_data = [
+            'name' => 'test1',
+            'email' => 'test1@email.com',
+            'password' => 'password'
+        ];
+
+        $this->assertDatabaseMissing('users', $valid_data);
+
+        $this->post(route('register'), $valid_data)->assertRedirect(route('home'));
+
+        $user = User::first();
+        $raw_password = $valid_data['password'];
+        unset($valid_data['password']);
+        $this->assertDatabaseHas('users', $valid_data);
+        $this->assertTrue(Hash::check($raw_password, $user->password));
+        $this->assertAuthenticatedAs($user);
     }
 }
