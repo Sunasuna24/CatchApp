543da071cf2be08d7bca4362a86cf5a1caf652fd
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-06 18:43:19 +0900

    ログイン画面にフォームを設置した

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,20 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="{{ route('login') }}" method="post">
+        @csrf
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+        <div>
+            <label for="email">メールアドレス</label>
+            <input type="email" name="email" id="email" value="{{ old('email') }}">
+        </div>
+        <div>
+            <label for="password">パスワード</label>
+            <input type="password" name="password" id="password">
+        </div>
+        <button type="submit">ログインする</button>
+    </form>
 </body>
 </html>
