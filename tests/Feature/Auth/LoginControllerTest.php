563895520bbe8fd3279caf2a51bc3386a2db751c
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-06 15:56:15 +0900

    ログイン処理周りのテストを追加した

--- tests/Feature/Auth/LoginControllerTest.php
+++ tests/Feature/Auth/LoginControllerTest.php
@@ -20,4 +20,19 @@ class LoginControllerTest extends TestCase
         $this->get(route('login'))->assertOk()->assertViewIs('auth.login');
         $this->actingAs($user)->get(route('login'))->assertRedirect(route('home'));
     }
+
+    /** @test */
+    function login_valid_user()
+    {
+        $valid_data = [
+            'email' => 'test1@email.com',
+            'password' => 'password'
+        ];
+        User::factory()->create($valid_data);
+        $user = User::first();
+
+        $this->get(route('home'))->assertRedirect(route('login'));
+        $this->post(route('login'), [])->assertRedirect(route('login'));
+        $this->actingAs($user)->post(route('login'), $valid_data)->assertRedirect(route('home'));
+    }
 }
