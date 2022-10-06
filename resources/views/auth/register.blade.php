1584fa14c355a5a37338e36f9a1ea613ae84bfac
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-06 15:31:43 +0900

    登録用のフォームを作成した

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,33 @@
     <title>Document</title>
 </head>
 <body>
-    <p>会員登録ページ</p>
+    <header></header>
+    <main>
+        <h2>会員登録</h2>
+        <form action="{{ route('register') }}" method="post">
+            @csrf
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+            <div>
+                <label for="name">ユーザー名</label>
+                <input type="text" name="name" id="name" value="{{ old('name') }}">
+            </div>
+            <div>
+                <label for="email">メールアドレス</label>
+                <input type="email" name="email" id="email" value="{{ old('email') }}">
+            </div>
+            <div>
+                <label for="password">パスワード</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <label for="password_confirmation">(確認用)パスワード</label>
+                <input type="password" name="password_confirmation" id="password_confirmation">
+            </div>
+            <button type="submit">送信する</button>
+        </form>
+    </main>
+    <footer></footer>
 </body>
 </html>
