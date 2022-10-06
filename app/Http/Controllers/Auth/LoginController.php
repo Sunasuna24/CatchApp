21cf90273c1a1b0c0629ea23954fec7e56043bae
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-06 18:40:34 +0900

    バリデーションを分離させた

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
@@ -13,12 +13,9 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $credentials = $request->validate([
-            'email' => ['required'],
-            'password' => ['required']
-        ]);
+        $credentials = $request->only(['email', 'password']);
 
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
