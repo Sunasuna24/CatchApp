20dc07ce4e35e83ace0280f26bc5fe2b23369230
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-08 00:03:53 +0900

    フォームを作成した

--- resources/views/release-note/create.blade.php
+++ resources/views/release-note/create.blade.php
@@ -8,5 +8,15 @@
 </head>
 <body>
     <h1>新しいリリースノートを投稿する</h1>
+    <form action="{{ route('release-note.create') }}" method="post">
+        @csrf
+        <div>
+            <input type="text" name="title" placeholder="タイトル" value="{{ old('title') }}">
+        </div>
+        <div>
+            <textarea name="detail" cols="30" rows="10" placeholder="リリースした内容">{{ old('detail') }}</textarea>
+        </div>
+        <button type="submit">送信する</button>
+    </form>
 </body>
 </html>
