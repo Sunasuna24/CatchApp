146f899d29d3ba68f4919a4815eb6f6c2fefaa87
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-08 23:45:25 +0900

    メニューを導入した

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,3 +1,8 @@
 <header>
     <h1>CatchApp</h1>
+    <ul>
+        <li><a href="{{ route('login') }}">ログインする</a></li>
+        <li><a href="{{ route('register') }}">会員登録する</a></li>
+        <li><a href="{{ route('feedback') }}">フィードバックを送る</a></li>
+    </ul>
 </header>
