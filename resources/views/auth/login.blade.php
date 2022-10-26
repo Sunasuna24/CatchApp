924331d2ff0ca1f883ec755e473c7817e0d5b1b1
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-10-26 16:12:15 +0900

    認証周りのviewにレイアウトを導入した

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="{{ route('login') }}" method="post">
-        @csrf
-        @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-        @endforeach
-        @if (session('failed_login'))
-        <p>{{ session('failed_login') }}</p>
-        @endif
-        <div>
-            <label for="email">メールアドレス</label>
-            <input type="email" name="email" id="email" value="{{ old('email') }}">
-        </div>
-        <div>
-            <label for="password">パスワード</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <button type="submit">ログインする</button>
-    </form>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'ログイン | CatchApp')
+
+@section('content')
+
+<form action="{{ route('login') }}" method="post">
+    @csrf
+    @foreach ($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+    @if (session('failed_login'))
+    <p>{{ session('failed_login') }}</p>
+    @endif
+    <div>
+        <label for="email">メールアドレス</label>
+        <input type="email" name="email" id="email" value="{{ old('email') }}">
+    </div>
+    <div>
+        <label for="password">パスワード</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <button type="submit">ログインする</button>
+</form>
+
+@endsection
