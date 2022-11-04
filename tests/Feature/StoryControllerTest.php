a88cf1c0e27597c64d73b3ad2a8f4d304d5cc3c0
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-11-04 11:47:37 +0900

    画像をアップロードするテストを書いた

--- tests/Feature/StoryControllerTest.php
+++ tests/Feature/StoryControllerTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class StoryControllerTest extends TestCase
@@ -19,5 +21,20 @@ class StoryControllerTest extends TestCase
 
         $this->get(route('story'))->assertRedirect(route('login'));
         $this->actingAs($user)->get(route('story'))->assertOk()->assertViewIs('story.post');
+    }
+
+    /** @test */
+    function post_stories()
+    {
+        User::factory()->create();
+        $user = User::first();
+
+        $this->post(route('story'), [])->assertRedirect(route('login'));
+
+        Storage::fake('stories');
+        $file = UploadedFile::fake()->image('sample_story.jpg');
+
+        $this->actingAs($user)->post(route('story'), ['story' => $file]);
+        Storage::disk('stories')->assertExists($file->hashName());
     }
 }
