061c1b54d858a0338f2e50fc102e42beaf3d460a
Sunasuna24 <sunasunayaka1218@gmail.com>
2022-11-04 11:07:06 +0900

    投稿画面のアクセス周りのテストを書いた

--- tests/Feature/StoryControllerTest.php
+++ tests/Feature/StoryControllerTest.php
@@ -2,17 +2,22 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class StoryControllerTest extends TestCase
 {
-    /** @test */
-    function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    function access_to_post_story_page()
+    {
+        User::factory()->create();
+        $user = User::first();
+
+        $this->get(route('story'))->assertRedirect(route('login'));
+        $this->actingAs($user)->get(route('story'))->assertOk()->assertViewIs('story.post');
     }
 }
